58929dc2994fee253a80ec66721087c747e7b29a
nirmalkumarr12 <nirmalkumar.r12@gmail.com>
2016-02-20 19:17:10 +0530

    Modified ALL FILES

---
+++ myscript.js
@@ -0,0 +1,10 @@
+var elems = document.getElementsByTagName("a");
+
+document.body.style.backgroundColor = "green";
+
+ for (var i = 0; i<elems.length; i++) {
+
+ 	elems[i].style.color = 'red';
+ 	elems[i].style.backgroundColor = 'pink';
+
+ }
